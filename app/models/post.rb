2b96f63071aeb06e7f397d25eaf1fe9a7983f4b4
Kurnia Muhamad <omenkzz@gmail.com>
2023-01-14 23:41:34 +0700

    Add validation to image activestorage

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,13 @@ class Post < ApplicationRecord
   has_one_attached :image
 
   validates :title, presence: true
+  validate :image?
 
   after_create_commit { broadcast_prepend_to :posts}
+
+  private
+
+  def image?
+    errors.add(:base, 'Please upload the image.') unless image.attached?
+  end
 end
