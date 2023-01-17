0542bab8f7bb4a505b8f188c4c41515e53469358
Kurnia Muhamad <omenkzz@gmail.com>
2023-01-17 21:42:17 +0700

    Update image validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,13 +2,17 @@ class Post < ApplicationRecord
   has_one_attached :image
 
   validates :title, presence: true
-  validate :image?
+  validate :image_content_type
 
   after_create_commit { broadcast_prepend_to :posts}
 
   private
 
-  def image?
-    errors.add(:base, 'Please upload the image.') unless image.attached?
+  def image_content_type
+    allowed = %w[image/jpeg image/jpeg image/webp image/png]
+    if allowed.exclude?(image.content_type)
+      errors.add(:image, message: 'must be a JPG, JPEG, WEBP or PNG')
+    end
   end
+
 end
